52b63a7bcddadc8ebf6126c705a2aa806742cdf6
Vladislav Gorodetsky <vladislav.gorodetsky@kaseya.com>
2022-12-23 16:39:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def greedisgood():
+    print('Hi, this is simple calculator,\nYou can use following actions: \n')
+
+
+if __name__ == '__main__':
+    greedisgood();
+
+# def print_hi(name):
+# Use a breakpoint in the code line below to debug your script.
+#    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
