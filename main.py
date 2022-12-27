5f9c5f4fe032d212116c60a34ed4ef7734521346
Vladislav Gorodetsky <vladislav.gorodetsky@kaseya.com>
2022-12-27 11:57:54 +0300

    Add main operations

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def greedisgood():
-    print('Hi, this is simple calculator,\nYou can use following actions: \n')
+    print('Hi, this is simple calculator,\nYou can use following actions: \n    +\n     -\n     *\n     /\n')
+    firstNumber = int(input('First number: '))
+    operation = input('Choose operation: ')
+    secondNumber = int(input('Second number: '))
+    if operation == '+':
+        print('Result: ', firstNumber+secondNumber)
+    elif operation == '-':
+        print('Result: ', firstNumber-secondNumber)
+    elif operation == '*':
+        print('Result: ', firstNumber*secondNumber)
+    elif operation == '/':
+        print('Result: ', round(firstNumber/secondNumber),3)
+    else:
+        print('Please, use available operation.')
+
+
 
 
 if __name__ == '__main__':
